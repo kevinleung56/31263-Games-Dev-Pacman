b253027e124a998a6e104c8bc08bdee5ebe29ebc
Kevin Leung <kevinleung56@gmail.com>
2021-09-18 11:15:16 +1000

    Add procedural generator script

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class LevelGenerator : MonoBehaviour
+{
+    int[,] levelMap =
+    {
+        {1,2,2,2,2,2,2,2,2,2,2,2,2,7},
+        {2,5,5,5,5,5,5,5,5,5,5,5,5,4},
+        {2,5,3,4,4,3,5,3,4,4,4,3,5,4},
+        {2,6,4,0,0,4,5,4,0,0,0,4,5,4},
+        {2,5,3,4,4,3,5,3,4,4,4,3,5,3},
+        {2,5,5,5,5,5,5,5,5,5,5,5,5,5},
+        {2,5,3,4,4,3,5,3,3,5,3,4,4,4},
+        {2,5,3,4,4,3,5,4,4,5,3,4,4,3},
+        {2,5,5,5,5,5,5,4,4,5,5,5,5,4},
+        {1,2,2,2,2,1,5,4,3,4,4,3,0,4},
+        {0,0,0,0,0,2,5,4,3,4,4,3,0,3},
+        {0,0,0,0,0,2,5,4,4,0,0,0,0,0},
+        {0,0,0,0,0,2,5,4,4,0,3,4,4,0},
+        {2,2,2,2,2,1,5,3,3,0,4,0,0,0},
+        {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
+    };
+    Tilemap proceduralTileMap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var tileMap = FindObjectOfType<Tilemap>();
+        //tileMap.DeleteCells(new Vector3Int(-9, 4, 0), )
+        //var position = tileMap.transform.position;
+
+
+        //proceduralTileMap = new Tilemap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
