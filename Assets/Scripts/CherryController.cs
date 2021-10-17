5fbf1b4991e759d4593a2d6d8b71a955949b2fa2
Kevin Leung <kevinleung56@gmail.com>
2021-10-17 16:26:37 +1100

    Finally get the moving cherry working

--- Assets/Scripts/CherryController.cs
+++ Assets/Scripts/CherryController.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class CherryController : MonoBehaviour
 {
+    private Tweener tweener;
+
+    [SerializeField]
+    private GameObject cherry;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tweener = GetComponent<Tweener>();
+
+        StartCoroutine(SpawnCherryCoroutine());
     }
 
     // Update is called once per frame
@@ -16,18 +23,55 @@ public class CherryController : MonoBehaviour
         
     }
 
-    public static int randomSpawnSizeX = 27; // top left -8.5, 4.5
-    public static int randomSpawnSizeY = 27; // bottom right 18.5, -23.5 // total size = (27, 28)
+    public static int tileMapSizeX = 27; // top left -8.5, 4.5
+    public static int tileMapSizeY = 28; // bottom right 18.5, -23.5 // total size = (27, 28)
+    public static Vector2 centreOfMapPosition = new Vector2(5f, -9.5f); // bottom right 18.5, -23.5 // total size = (27, 28)
     // centre point is 5, -9.5
 
 
-    IEnumerable SpawnCherryCoroutine()
+    IEnumerator SpawnCherryCoroutine()
     {
-        var cameraViewHeight = Camera.main.orthographicSize;
-        var cameraViewWidth = Camera.main.orthographicSize * Camera.main.aspect;
+        while (true)
+        {
+            yield return new WaitForSeconds(10);
 
-        var map = LevelGenerator.levelMap;
+            StartCoroutine(GenerateCherryAndTween());
+        }
+    }
 
-        yield return new WaitForSeconds(10);
+    IEnumerator GenerateCherryAndTween()
+    {
+        var randomPositionOutOfBounds = GenerateRandomPointOutOfBounds();
+        var newCherry = Instantiate(cherry, randomPositionOutOfBounds, Quaternion.identity);
+        var finalPosition = (centreOfMapPosition - randomPositionOutOfBounds) + centreOfMapPosition;
+        tweener.AddTween(newCherry.transform, newCherry.transform.position, centreOfMapPosition, 5f);
+        yield return new WaitForSeconds(5f);
+
+        tweener.AddTween(newCherry.transform, newCherry.transform.position, finalPosition, 5f);
+        yield return new WaitForSeconds(5f);
+
+        Destroy(newCherry);
+    }
+
+    Vector2 GenerateRandomPointOutOfBounds()
+    {
+        var randomPointLeftOfCameraX = Random.Range(-20.0f, -10.0f);
+        var randomPointRightOfCameraX = Random.Range(20.0f, 30.0f);
+        var randomPointAboveCameraY = Random.Range(-10.0f, 30.0f);
+        var randomPointBelowCameraY = Random.Range(-10.0f, 30.0f);
+
+        var takeLeftOrRight = Random.Range(0, 2);
+        var takeUpOrDown = Random.Range(0, 2);
+
+        Vector2 randomPosition;
+        float randomPositionX;
+        float randomPositionY;
+
+        randomPositionX = takeLeftOrRight == 0 ? randomPointLeftOfCameraX : randomPointRightOfCameraX;
+        randomPositionY = takeUpOrDown == 0 ? randomPointAboveCameraY : randomPointBelowCameraY;
+
+        randomPosition = new Vector2(randomPositionX, randomPositionY);
+
+        return randomPosition;
     }
 }
