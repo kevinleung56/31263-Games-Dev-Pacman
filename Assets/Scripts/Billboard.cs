f27d498e1d33f74b6fd7f092ab187e49d6deab8f
Kevin Leung <kevinleung56@gmail.com>
2021-11-05 00:06:48 +1100

    Look at camera better

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -1,20 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
-    private Camera gameCamera;
-
     // Start is called before the first frame update
     void Start()
     {
-        gameCamera = FindObjectOfType<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(2 * transform.position - gameCamera.transform.position);
+        transform.forward = Camera.main.transform.forward;
     }
 }
